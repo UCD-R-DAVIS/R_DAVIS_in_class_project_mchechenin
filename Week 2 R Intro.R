20f833108a843bf5aac8b95a1be6ca1a37b67395
Mark Henry <legomastermark@campus-098-124.ucdavis.edu>
2024-10-03 13:58:47 -0700

    Week 2 Intro Work

---
+++ Week 2 R Intro.R
@@ -0,0 +1,26 @@
+
+log(x=2, base = 4)
+
+mynumber<-6
+
+mynumber==5
+
+mynumber > 4
+
+othernumber <- -3
+
+mynumber * othernumber
+
+elephant1_kg <- 3492
+
+elephant2_lb <- 7757
+
+elephant1_lb <- elephant1_kg *2.2
+
+elephant1_lb
+
+elephant2_lb > elephant1_lb
+
+myelephants <- c(elephant1_lb, elephant2_lb)
+
+which(myelephants == max(myelephants))
