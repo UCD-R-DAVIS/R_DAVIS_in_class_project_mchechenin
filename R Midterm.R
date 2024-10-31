3dfa3089ba716a03dfad6448e4b6c2a954315700
Mark Henry <legomastermark@campus-007-041.ucdavis.edu>
2024-10-31 13:16:16 -0700

    Midterm

---
+++ R Midterm.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(dplyr)
+
+file <- 'https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/refs/heads/main/data/tyler_activity_laps_10-24.csv'
+
+data <- read_csv(file)
+
+summary(data)
+
+colnames(data)
+      
+running <- data %>%
+filter(sport == "running") %>%
+filter(minutes_per_mile < 10 & minutes_per_mile > 5) %>%
+mutate(pace = case_when(
+minutes_per_mile < 6 ~ "fast",
+is.na(minutes_per_mile) ~ NA_character_,
+minutes_per_mile >= 6 & minutes_per_mile < 8 ~ "medium",
+TRUE ~ "slow")) %>%
+mutate(form = case_when(
+year == 2024 ~ "new",
+is.na(year) ~ NA_character_,
+year == 2023 ~ "old",
+TRUE ~ NA_character_))
+
+
+running %>%
+filter(!is.na(minutes_per_mile)) %>%
+  group_by(minutes_per_mile, form)
+summarize(average_spm = mean(steps_per_minute)) %>%
+pivot_wider(names_from = "form", values_from =  "minutes_per_mile") %>%
+arrange(Control)
+
